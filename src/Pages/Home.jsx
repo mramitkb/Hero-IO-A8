f567e81ad3dab231cb891c7e475861927947522b
Amit KB <mr.amitkb@gmail.com>
2026-03-18 13:46:20 +0600

    Dashboard Completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Banner from '../Components/Header/Banner/Banner';
+import Dashboard from '../Components/Dashboard/Dashboard';
 
 const Home = () => {
     return (
         <div>
             <div className='bg-[#F5F5F5]'>
                 <Banner></Banner>
+            </div>
+            <div className='bg-linear-to-br from-[#632EE3] to-[#9D61F2] py-12 text-white'>
+                <Dashboard></Dashboard>
             </div>
         </div>
     );
